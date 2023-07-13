20a89f61e8d460e633faae087b34136fadedb4c5
Simo Khanyi <simokhanyi@gmail.com>
2023-07-13 09:53:22 -0700

    C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * This program prints multilingual puzzle
  * main - main block
  * return: 0
@@ -10,5 +10,5 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 
-return 0;
+return (0);
 }
