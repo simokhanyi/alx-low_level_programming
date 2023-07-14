c77944f87adbd724c43ec0d2b3891774d694030f
Simo Khanyi <simokhanyi@gmail.com>
2023-07-14 06:40:47 -0700

    If statements

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/*main - Main Entry*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand(5) - RAND_MAX / 2;
+	/* your code goes there */
+	if( a < 0 ) 
+
+     	/* if condition is true then print the following */
+      	printf("Value of a is \n" );
+   	 else if( a == 20 ) 
+      	/* if else if condition is true */
+      	printf("Value of a is 20\n" );
+   	else if( a == 30 )
+      	/* if else if condition is true  */
+      	printf("Value of a is 30\n" );
+	return (0);
+}
