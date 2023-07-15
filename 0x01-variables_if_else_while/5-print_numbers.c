4a7b71246d6e10018d800dbbb3775cf51c95fd77
Simo Khanyi <simokhanyi@gmail.com>
2023-07-14 18:44:51 -0700

    If statements

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: program prints all single digit numbers
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+	printf("%d", num);
+	num += 1;
+	}
+	printf("\n");
+	return (0);
+}
