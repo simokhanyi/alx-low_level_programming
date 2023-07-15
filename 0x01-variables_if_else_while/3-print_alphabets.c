9016f7ace432cce6cd1dead1d7e73fc44481d67e
Simo Khanyi <simokhanyi@gmail.com>
2023-07-14 18:07:19 -0700

    If statements

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,17 @@
 int main(void)
 {
 	int lowerCase = 'a';
+	int upperCase = 'A';
 
 	while (lowerCase <= 'z')
 	{
 	putchar(lowerCase);
 	lowerCase += 1;
+	}
+	while (upperCase <= 'z')
+	{
+	putchar(upperCase);
+	upperCase += 1;
 	}
 	putchar ('\n');
 	return (0);
