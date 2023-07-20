310851fcc00bb11557d02aab451f1540ec91569c
Simo Khanyi <simokhanyi@gmail.com>
2023-07-20 06:55:08 -0700

    More functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,12 @@
 
 void print_numbers(void)
 {
-	unsigned int n1;
+	unsigned int n1, n;
 
-	if (n1 < 0)
+	if (n < 0)
 	{
 		n1 = -n;
-		_putchar('_');
+		_putchar('-');
 	} else
 	{
 		n1 = n;
@@ -20,7 +20,7 @@ void print_numbers(void)
 
 	if (n1 / 10)
 	{
-		print_numbers(n1 / 10);
+		print_number(n1 / 10);
 	}
 
 	_putchar((n1 % 10) + '0');
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,10 +4,10 @@
  * mul - miltiply 2 int
  * @a: first int
  * @b: second int
- * Return: addition of the multiplicattion
+ * Return: sum of the multiplicattion
  */
 
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
