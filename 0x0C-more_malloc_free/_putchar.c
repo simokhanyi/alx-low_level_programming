baad3e9d3cccfbb468d23f80390b3c4a3beab111
Simo Khanyi <simokhanyi@gmail.com>
2023-08-10 21:55:08 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On Success 1
+ * On error, -1 is returned, and ermo is set appropriatley
+ */
+
+int _putchar(char c)
+{
+        return(write(1, &c, 1));
+}
