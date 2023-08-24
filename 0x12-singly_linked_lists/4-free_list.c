b36f1261c0a12c50b6e3656832ea31179b2ea8d9
Simo Khanyi <simokhanyi@gmail.com>
2023-08-24 13:15:22 -0700

    lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+
+	while (current != NULL)
+	{
+		list_t *temp = current;
+
+		current = current->next;
+
+		free(temp);
+	}
+}
