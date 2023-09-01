56eede4650bb80e28a910c4b08d390618613abe5
Simo Khanyi <simokhanyi@gmail.com>
2023-08-31 19:20:31 -0700

    Bit manipulatio

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would
+ * need to flip to get from one number to another
+ * @n: number one.
+ * @m: number two.
+ *
+ * Return: number of bits.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int xor_result, count;
+
+	xor_result = n ^ m;
+	count = 0;
+
+	while (xor_result)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
+
+	return (count);
+}
