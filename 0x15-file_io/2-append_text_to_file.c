1c40158d9a6a27b326c54cd794899e34d48d59c0
Simo Khanyi <simokhanyi@gmail.com>
2023-09-04 08:24:07 -0700

    File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,8 +25,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	result = fprintf(file, "%s", text_content);
-
-	fclose(file);
+	
+		fclose(file);
 
 	if (result < 0)
 	{
