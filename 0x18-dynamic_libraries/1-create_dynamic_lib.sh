2d46c1345e979bfb9457fda221347d86b0f6f0c9
Simo Khanyi <simokhanyi@gmail.com>
2023-10-16 15:28:25 -0700

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -wall -wextra -werror -pendatic -c -fPIC *.c
+gcc -Wall -Wextra -Werror -pendatic -c -fPIC *.c
 dcc -shared -0 liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
