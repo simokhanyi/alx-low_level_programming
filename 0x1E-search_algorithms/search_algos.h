dae9d81dedf2ff9d7b37812b17a406c3874b0584
Simo Khanyi <simokhanyi@gmail.com>
2024-02-07 21:47:59 -0800

    Search algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -41,7 +41,7 @@ typedef struct skiplist_s
 
 /* Function prototypes */
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t low, size_t high, int value);
+int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
